6a0009be4ec36e450c5bd478356beef3821080b4
Teofilo Messias Filho <re045002@qintess.com>
2023-10-02 17:11:57 -0300

    Seção02: Aula04 - Arquitetura Cliente Servidor

---
+++ Secao02-Bases-da-programacao-Web/aula04_arquitetura_cliente_servidor.py
@@ -0,0 +1,6 @@
+"""
+Arquitetura	Cliente-Servidor
+Estrutura de computação distribuída que distribui tarefas e cargas de trabalho entre fornecedores de
+serviço, chamados de servidores, e os requerentes dos serviços, chamados de clientes.
+
+"""
