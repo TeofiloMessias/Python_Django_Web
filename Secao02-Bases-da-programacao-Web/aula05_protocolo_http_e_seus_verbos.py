8be8c2f4c72bbe84f139e1399a0405e5bca78d5b
Teofilo Messias Filho <re045002@qintess.com>
2023-10-02 17:23:34 -0300

    Seção02: Aula05 - Protocolos HTTP e seus verbos

---
+++ Secao02-Bases-da-programacao-Web/aula05_protocolo_http_e_seus_verbos.py
@@ -0,0 +1,21 @@
+"""
+Protocolo	HTTP	e	seus	verbos
+Um dos protocolos mais importantes e utilizados no mundo, o Protocolo de Transferência de Hipertexto
+é o que usamos todos os dias, mesmo sem saber, para fazer acesso a todos os websites e redes
+sociais que conhecemos.
+Os serviços que implementam este protocolo, chamados de serviços HTTP, ou servidores HTTP, se
+comunicam nas portas 80 para HTTP ou 443 para HTTPS.
+Você encontra toda a referência para este protocolo, atualmente na versão 2, na RFC 7540[1] que foi
+definida pela IETF - Internet Engineering Task Force.
+[1]https://tools.ietf.org/html/rfc7540
+
+Protocolo	HTTP	e	seus	verbos
+O protocolo HTTP define métodos para indicar a ação desejada a ser realizada no recurso identificado.
+Estes métodos são chamados de verbos HTTP quando utilizados como aplicações web.
+
+ - Get -> Busca recursos / Cache
+ - Post -> Cria um novo recurso
+ - Put/Patch -> Atualizar um recurso existente
+ - Delete -> Remove um recurso
+
+"""
