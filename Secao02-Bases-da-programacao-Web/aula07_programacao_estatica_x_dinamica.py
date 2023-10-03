16593d106eb17f84762f45641bca7db4de4de3e8
Teofilo Messias Filho <re045002@qintess.com>
2023-10-03 10:49:32 -0300

    Seção02: Aula07 - Programação estatica e dinamica

---
+++ Secao02-Bases-da-programacao-Web/aula07_programacao_estatica_x_dinamica.py
@@ -0,0 +1,8 @@
+"""
+Programação	Estática	x	Dinâmica
+Usamos o termo programação estática ou dinâmica para nos referir do uso de linguagens de
+programação que não são processados no servidor (estática) ou que são processados no servidor
+(dinâmica).
+Como exemplos de linguagens estáticas temos HTML, CSS e JavaScript, etc.
+Como exemplos de linguagens dinâmicas temos Python, Java, Ruby, PHP, C#, JavaScript, etc
+"""
