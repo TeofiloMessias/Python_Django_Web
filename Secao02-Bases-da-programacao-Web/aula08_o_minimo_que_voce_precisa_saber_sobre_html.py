dc9551ae1cc1f51236b58d7d2a32080388cd9e74
Teofilo Messias Filho <re045002@qintess.com>
2023-10-03 14:42:21 -0300

    Seção02: Aula08 - O minimo que você precisa saber sobre HTML

--- Secao02-Bases-da-programacao-Web/aula08_o_minimo_que_voce_precisa_saber_sobre_html.py
+++ Secao02-Bases-da-programacao-Web/aula08_o_minimo_que_voce_precisa_saber_sobre_html.py
@@ -1,3 +1,46 @@
 """
+HTML é a sigla para Hypertext Markup Language, ou Linguagem de Marcação para Hipertexto.
+Utilizamos HTML para criar estruturas para páginas web: seções, parágrafos, cabeçalhos, divisões,
+links e etc.
+Toda página na internet que você acessa se você clicar com o botão do mouse no navegador e pedir
+para ver o código fonte será mostrado o conteúdo HTML da página
+
+Ao trabalhar com HTML nós codificamos estruturas (tags e atributos) para marcar a página de um
+site, por isso o HTML é chamado de linguagem de marcação. Um arquivo HTML tem extensão ‘.html’.
+
+De forma geral, uma página web é dividida em 3 área principais: header, body e footer.
+Uma página web é renderizada pelo navegador de cima para baixo, linha a linha.
+Header: Usado para tags de informação sobre o
+site, aplicação de estilos css e etc.
+Body: Conteúdo do site que é apresentado ao
+cliente.
+Footer: Geralmente usado para carregamento de
+scripts Javascript e apresentação de dados de
+rodapé.
+OBS: O footer fica dentro da área de conteúd
+
+As tags HTML são subdivididas em dois grupos principais:
+Elementos de bloco: Utilizam todo o espaço disponível e começam uma nova linha no documento.
+Elementos em linha: Utilizam apenas o espaço necessário e não criam uma quebra de linha.
+OBS: Iremos entender melhor a diferença entre eles nos exemplos desta aula
+
+Tags mais utilizadas nos elementos de bloco:
+<html> </html>: É o elemento de maior nível que está em todas as páginas HTML.
+<head> </head>: Possui informações do meta como título e charset da página.
+<body> </body>: Engloba todos os elementos que são mostrados na página.
+<h1> </h1>, <h2> </h2>, <h3> </h3>, <h4> </h4>, <h5> </h5>, <h6> </h6>: Cabeçalhos com níveis de 1 a 6, onde o 1 é o
+maior nível e o 6 o menor.
+<div> </div>: Divisões de seções de conteúdo.
+<p> </p>: Parágrafo.
+<blockquote> </blockquote>: Bloco de citações.
+<ol> </ol>: Listas ordenadas.
+<ul> </ul> Listas não ordenadas.
+<li> </li>: Item de listas
+
+Tags mais utilizadas nos elementos de linha:
+<strong> </strong>: Renderiza um texto em negrito.
+<em> </em>: Renderiza um texto em itálico.
+<a> </a>: Cria um link para outra página ou documento.
+<img>: Adiciona imagem na página.
 
 """
