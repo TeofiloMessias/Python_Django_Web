87d5cfd7d1f4a5dbe1265ab66b98c3f0d8c15367
Teofilo Messias Filho <re045002@qintess.com>
2023-10-03 09:48:23 -0300

    Seção02: Aula06 - Programação Backend x Frontend x FUll-Stack

---
+++ Secao02-Bases-da-programacao-Web/aula06_programacao_backend_x_frontend_x_full-stack.py
@@ -0,0 +1,50 @@
+"""
+Programação	Backend	x	Frontend	x	Full-Stack
+
+No mundo do desenvolvimento de software existem três termos muito comuns que provavelmente
+você já se deparou: frontend, backend e full-stack.
+É muito comum vermos vagas de empregos com os títulos:
+Programador Frontend;
+Programador Backend;
+Programador FullStack;
+Mas qual a diferença entre elas?
+
+Frontend
+Quando falamos em frontend, estamos falando da aplicação responsável pela apresentação do
+software, chamado de client-side (lembra do cliente-servidor?).
+Se tratando de aplicações web, é exatamente o código do sistema que roda no navegador.
+Um desenvolvedor frontend, geralmente, trabalha com linguagens como HTML, CSS e JavaScript,
+além de frameworks e bibliotecas, como Angular, React, Vue.JS, etc.
+Embora estes programadores não precisem conhecer como desenvolver código de backend, é
+extremamente importante que eles conheçam os fundamentos sobre a arquitetura de software, afinal
+de contas, o código que eles produzem faz parte de um todo e se comunica com o backend
+
+Frontend
+Desenvolvedores frontend não lidam diretamente com banco de dados, servidores de aplicação
+complexos e várias outras coisas que só quem trabalha com backend conhece.
+Os desafios para quem trabalha com frontend são outros: Criar páginas ou telas com boa usabilidade
+e carregamento rápido, garantir o funcionamento nos diferentes navegadores, integrar com os serviços
+do backend, etc
+
+Backend
+O código que é escrito para rodar no servidor (HTTP), também conhecido como server-side, é o
+backend.
+É o backend que fornece e garante todas as regras de negócio, acesso a banco de dados, segurança
+e escalabilidade.
+Embora o frontend também possa ter algumas regras e validações, é o backend que deve garantir a
+integridade dos dados
+
+Backend
+Desenvolvedores que se especializam apenas em backend, geralmente, não são muito bons em
+deixar páginas web bonitas e com boa usabilidade, mas são melhores em regras de negócio, banco de
+dados e todas as cosias que rodam no servidor.
+Estes desenvolvedores trabalham com linguagem de programação como, Python, Java, C#, Ruby ou
+até mesmo JavaScript
+
+FullStack
+Quem trabalha tanto como frontend quanto como backend é conhecido como FullStack.
+Este é um tipo de profissional mais completo, que pode entregar um projeto do início ao fim, sem
+necessariamente precisar de ajuda de outra pessoa para criar uma parte do sistema.
+De certa foram é um profissional mais valorizado no mercado, especialmente se for realmente bom em
+frontend e backen
+"""
