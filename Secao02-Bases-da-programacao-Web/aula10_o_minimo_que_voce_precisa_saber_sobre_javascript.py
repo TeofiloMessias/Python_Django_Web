1faf0873826689689c281e400614abf55361d39a
Teofilo Messias Filho <re045002@qintess.com>
2023-10-04 09:43:50 -0300

    Seção02: Aula02 - Estilos Css e Html - Aprendendo o minimo

---
+++ Secao02-Bases-da-programacao-Web/aula10_o_minimo_que_voce_precisa_saber_sobre_javascript.py
@@ -0,0 +1,13 @@
+"""
+A linguagem JavaScript foi criada por Brendan Eich na empresa Netscape.
+Atualmente é mantida pela European Computer Manufacturer’s Association e seu nome “oficial" é
+ECMAScript, mas continua sendo mais conhecida, principalmente pelos mais leigos, como JavaScript.
+OBS: JavaScript não é Java!
+
+O JavaScript completa a tríade da Web se encarregando dos comportamentos e ações das páginas.
+HTML -> Estrutura;
+CSS -> Estilos;
+JavaScript -> Comportamento/ações;
+Vamos entender melhor sobre comportamentos/ações nos exemplos a seguir
+
+"""
