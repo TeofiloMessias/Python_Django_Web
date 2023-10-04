d5cb35b97fb20f0704c0c1bb51ae0790b5e5995d
Teofilo Messias Filho <re045002@qintess.com>
2023-10-04 11:57:04 -0300

    Seção02: Aula16 - Recapitulando e finalizando a seção

---
+++ Secao02-Bases-da-programacao-Web/aula16_recapitulando.py
@@ -0,0 +1,15 @@
+"""
+ - Arquitetura Cliente-Servidor
+ - Protocolo HTTP e seus verbos
+ - Programação Backend x Frontend x Full-stack
+ - Programação Estatica x Dinâmica
+ - Exercício de programação1: Mão na massa
+ - O minimo que você precisa saber sobre HTML
+ - O minimo que você precisa saber sobre CSS
+ - O minimo que você precisa saber sobre javaScript
+ - O minimo que você precisa saber sobre frameworks
+ - A salvação de programadores backend: Templets
+ - Instalando e configurando o git no Windows
+ - Instalando e configurando o git no Linux
+ - Instalando e configurando o git no Mac
+"""
