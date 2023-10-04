d5f1f26ef6e7b3062da9d9a39543421208a11cbd
Teofilo Messias Filho <re045002@qintess.com>
2023-10-04 10:15:45 -0300

    Seção02: Aula09 - O minimo que você precisa saber sobre Frameworks

---
+++ Secao02-Bases-da-programacao-Web/aula11_o_minimo_que_voce_precisa_saber_sobre_frameworks.py
@@ -0,0 +1,9 @@
+"""
+O	mínimo	que	você	precisa	saber	sobre	frameworks	frontend
+Um framework frontend nada mais é do que um conjunto de ferramentas que oferecem diversos
+recursos para desenvolvedores frontend, ou seja, para realizar as criações de html, css e javascript.
+Um dos frameworks mais famosos e utilizamos atualmente é o Bootstrap[1] criado pela empresa por
+traz do Twitter e disponibilizado para toda a comunidade.
+
+
+"""
