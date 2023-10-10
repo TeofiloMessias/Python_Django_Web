bde91f92367d4ee939c8407410758b967e8946bd
Teofilo Messias Filho <re045002@qintess.com>
2023-10-10 13:30:46 -0300

    Secao03: Iniciando a seção 3

---
+++ Secao03-Django-Framework-Basico/aula17_o_que_vamos_aprender_nesta_secao.py
@@ -0,0 +1,10 @@
+"""
+Configuração do Django e instalação e publicar o projeto.
+Configurar rota
+Criar ambiente virtual
+Criar projeto com SQLite3
+Padrões MTV do Django
+Criando um ambiente virtual istalando django
+Criando um projeto django com sqlite3 estrutura
+Criando uma aplicacao django conhecendo estrutura
+"""
