9eb57013b6bfdaa26dfbff2fde6cd25e2db4f2de
Teofilo Messias Filho <re045002@qintess.com>
2023-10-10 13:31:46 -0300

    Secao03: Criando um ambiente virtual - aula 18

---
+++ Secao03-Django-Framework-Basico/aula18_criando_um_ambiente_virtual_istalando_django.py
@@ -0,0 +1,13 @@
+"""
+Comandos de instalação do django
+
+- pip install django
+- pip freeze > requirements.txt
+    ( cria uma arquivo txt com versões do django)
+- django-admin startproject django1 .
+    cria um projeto dentro do diretorio
+- django-admin startapp core
+    cria uma aplicação
+
+
+"""
