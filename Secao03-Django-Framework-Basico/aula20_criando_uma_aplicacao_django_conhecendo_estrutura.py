0d75928a0b9756953a76fd90ee4166b743760f67
Teofilo Messias Filho <re045002@qintess.com>
2023-10-10 13:33:24 -0300

    Secao03: Conhecendo estrutura de uma aplicação - aula 20

---
+++ Secao03-Django-Framework-Basico/aula20_criando_uma_aplicacao_django_conhecendo_estrutura.py
@@ -0,0 +1,28 @@
+"""
+
+########## Arquivos dentro do diretorio: ##########
+
+__init__ -> transforma um diretorio em pacote python
+
+settings -> Chega o diretorio base atraves do Base_dir
+	 -> Secret_Key gera senha ou chave criptrografada
+	 -> Debug True mudar para falso depois que finalizar o desenvolvimento
+
+ALLOWED_HOST - depois mudar para o provedor
+
+Middleware Entermediario que fica verificando a segurança entre outras coisas
+
+ROOT_URLCONF -> configuração de url
+
+TEMPLETES -> frontend parte visivel para o usuário
+
+WSGI_APPLICATION ->
+
+DATABASES -> banco de dados SQlite3
+
+Dentro do CORE  o pacote 'Migrations' é responsavel pela migragrações de banco de dados.
+	versões do banco de dados.
+
+views -> abre os templetes
+
+"""
