ad194ae6006f7cb63e5440a682a9026d85179fde
Teofilo Messias Filho <re045002@qintess.com>
2023-10-10 13:34:38 -0300

    Secao03: Projeto vs Aplicação - aula 21

---
+++ Secao03-Django-Framework-Basico/aula21_projeto_aplicacao.py
@@ -0,0 +1,47 @@
+"""
+Django:	Projeto	x	Aplicações
+Quando trabalhamos com o Django Framework devemos levar em conta a forma de trabalho do mesmo
+que possui um projeto que engloba o todo e aplicações plugáveis.
+Ou seja, em um projeto Django, podemos ter várias aplicações, cada uma com sua tarefa específica, e
+estas aplicações depois de criadas podem ser utilizadas por outros projetos.
+
+Quando trabalhamos com o Django Framework devemos levar em conta a forma de trabalho do mesmo
+que possui um projeto que engloba o todo e aplicações plugáveis.
+Ou seja, em um projeto Django, podemos ter várias aplicações, cada uma com sua tarefa específica, e
+estas aplicações depois de criadas podem ser utilizadas por outros projetos.
+rojeto xyz
+Aplicação 1
+Aplicação 2
+Aplicação 3
+Aplicação 4
+Aplicação 5
+Aplicação 6
+Aplicação 7
+Aplicação 8
+
+
+Quando trabalhamos com o Django Framework devemos levar em conta a forma de trabalho do mesmo
+que possui um projeto que engloba o todo e aplicações plugáveis.
+Ou seja, em um projeto Django, podemos ter várias aplicações, cada uma com sua tarefa específica, e
+estas aplicações depois de criadas podem ser utilizadas por outros projetos.
+
+E-commerce
+Clientes
+Pagamentos
+Carrinho
+Pedidos
+Entrega
+Produtos
+Promoções
+Cupons
+
+Estoque
+Produtos Pedidos
+
+Quando trabalhamos com o Django Framework devemos levar em conta a forma de trabalho do mesmo
+que possui um projeto que engloba o todo e aplicações plugáveis.
+Ou seja, em um projeto Django, podemos ter várias aplicações, cada uma com sua tarefa específica, e
+estas aplicações depois de criadas podem ser utilizadas por outros projetos.
+
+
+"""
